9ed50c1ca8fa32f07e921538b5f005519eab5bc8
iamanolive <theolivequeen.xyz@gmail.com>
2024-07-24 23:35:15 +0300

    hello world

---
+++ 1-python-programming/01-hello-world/initials.py
@@ -0,0 +1,10 @@
+# anoushka mahajan
+# fun fact: i have a cat
+
+print("   A    M   M ")
+print("  A A   MM MM ")
+print(" A   A  MM MM ")
+print(" AAAAA  M M M ")
+print(" A   A  M   M ")
+print(" A   A  M   M ")
+print(" A   A  M   M ")
